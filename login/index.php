5b94251f8d3edf9c6a15d0045db703153d43111c
Josue-UMSA <jgutierrezm5@umsa.bo>
2021-04-04 16:40:41 -0400

    Update index.php

--- login/index.php
+++ login/index.php
@@ -1,13 +1,13 @@
 <?php 
 include '../parciales/cabecera1.inc.php';
 include '../parciales/cabecera2.inc.php';
-if (!isset($_SESSION['usr'])):
+if (!isset($_SESSION['usr'])){
 	include 'cuerpo.inc.php';
 	include '../parciales/footer1.inc.php';
 ?>
 	<script src="../js/login.js"></script>
-<?php else: 
-	header('http://localhost/PrimerParcial324/');
-	 endif; 
+<?php }else{
+	 header('http://localhost/PrimerParcial324/index.php');
+	 }
 	 include '../parciales/footer2.inc.php'; 
 ?>
