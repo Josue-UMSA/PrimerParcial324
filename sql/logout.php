3506db3e34897ede8bd894a846cf8c26c07ee8bb
Josue-UMSA <jgutierrezm5@umsa.bo>
2021-04-06 18:28:56 -0400

    Delete logout.php

--- sql/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-if(isset($_POST['op'])){
-    session_destroy();
-    header('Location: http://localhost/PrimerParcial324/index.php');
-}
-header('Location: http://localhost/PrimerParcial324/index.php');
-?>
